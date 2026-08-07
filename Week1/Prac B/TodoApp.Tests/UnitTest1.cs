6a405d0286e2f98120e07fb647463d9cdc0c7fbd
Lu Yuchen <158829614@qq.com>
2026-08-07 16:11:21 +0930

    Test code :)

--- Week1/Prac B/TodoApp.Tests/UnitTest1.cs
+++ Week1/Prac B/TodoApp.Tests/UnitTest1.cs
@@ -1,10 +1,22 @@
-﻿namespace TodoApp.Tests;
+﻿using Xunit;
 
-public class UnitTest1
+namespace TodoApp.Tests;
+
+public class PersonTests
 {
     [Fact]
-    public void Test1()
+    public void FullName_ReturnsExpectedFormat()
     {
+        Person person = new Person("Yuchen", "Lu", 20);
 
+        Assert.Equal("Lu, Yuchen", person.FullName());
+    }
+
+    [Fact]
+    public void IsAdult_ReturnsTrue_WhenAge18OrMore()
+    {
+        Person person = new Person("Yuchen", "Lu", 20);
+
+        Assert.True(person.IsAdult());
     }
 }
