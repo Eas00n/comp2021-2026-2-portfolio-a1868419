30cb43a339ed74a5df8f82992090ebc31476ce20
Lu Yuchen <158829614@qq.com>
2026-08-13 15:00:30 +0930

    add Deposit

--- Week2/Prac B/BankAccount.cs
+++ Week2/Prac B/BankAccount.cs
@@ -2,4 +2,8 @@ public class BankAccount
 {
     public string Owner { get; set; }
     public decimal Balance { get; set; }
+    public void Deposit(decimal amount)
+    {
+        Balance += amount;
+    }
 }
