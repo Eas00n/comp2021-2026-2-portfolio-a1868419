7cf0f493a7325b2277abc2598b94c916b1adf0d1
Lu Yuchen <158829614@qq.com>
2026-08-17 13:23:36 +0930

    Copy Topic 2 as a base class

---
+++ Week3/Prac A/BankAccount/BankAccount.cs
@@ -0,0 +1,26 @@
+public class BankAccount
+{
+    public string Owner { get; set; }
+    public decimal Balance { get; set; }
+
+    public BankAccount(string owner, decimal balance)
+    {
+        Owner = owner;
+        Balance = balance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        Balance += amount;
+    }
+
+    public virtual void Withdraw(decimal amount)
+    {
+        if (amount > Balance)
+        {
+            throw new ArgumentException("You do not have enough money in your account.");
+        }
+
+        Balance -= amount;
+    }
+}
