c96be4264f2e241fd7ac112b9d73ee1b1cca7687
Lu Yuchen <158829614@qq.com>
2026-08-17 13:34:47 +0930

    Add SavingsAccount base

--- Week3/Prac A/BankAccount/SavingsAccount.cs
+++ Week3/Prac A/BankAccount/SavingsAccount.cs
@@ -1,5 +1,14 @@
 public class SavingsAccount : BankAccount
 {
     public decimal InterestRate { get; set; }
-    
+    public SavingsAccount(string owner, decimal balance, decimal interestRate)
+        : base(owner, balance)
+    {
+        InterestRate = interestRate;
+    }
+
+    public void ApplyInterest()
+    {
+        Balance += Balance * InterestRate;
+    }
 }
