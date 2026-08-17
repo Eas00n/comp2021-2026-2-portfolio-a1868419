2628bb93cd418cfce82f4027bdde499d6889e8f9
Lu Yuchen <158829614@qq.com>
2026-08-17 13:47:29 +0930

    Add test code

--- Week3/Prac A/BankAccount/Program.cs
+++ Week3/Prac A/BankAccount/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿SavingsAccount savings = new SavingsAccount("Yuchen", 1000m, 0.05m);
+savings.ApplyInterest();
+
+CheckingAccount checking = new CheckingAccount("Yuchen", 1000m, 10m);
+checking.Withdraw(100m);
+
+Console.WriteLine($"Savings Account: {savings.Owner}, Balance: ${savings.Balance:F2}");
+Console.WriteLine($"Checking Account: {checking.Owner}, Balance: ${checking.Balance:F2}");
