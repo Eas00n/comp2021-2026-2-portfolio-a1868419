868177d822c204413830f72db9c933168c04700a
Lu Yuchen <158829614@qq.com>
2026-08-20 16:06:17 +0930

    Add Test folder and UnitTest1

---
+++ Week3/Prac B/EmployeeApp.Tests/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+
+public class EmployeeTests
+{
+    [Fact]
+    public void FullTimeEmployee_CalculatePay_ReturnsNetPay()
+    {
+        var employee = new FullTimeEmployee("Bill", 60000m);
+
+        decimal pay = employee.CalculatePay();
+
+        Assert.Equal(48000m, pay);
+    }
+
+    [Fact]
+    public void FullTimeEmployee_GenerateReport_ReturnsCorrectReport()
+    {
+        var employee = new FullTimeEmployee("Bill", 60000m);
+
+        string report = employee.GenerateReport();
+
+        Assert.Contains("Bill", report);
+        Assert.Contains("60000.00", report);
+        Assert.Contains("48000.00", report);
+    }
+
+    [Fact]
+    public void Contractor_CalculatePay_ReturnsNetPay()
+    {
+        var contractor = new Contractor("Fred", 50m, 40m);
+
+        decimal pay = contractor.CalculatePay();
+
+        Assert.Equal(1600m, pay);
+    }
+
+    [Fact]
+    public void Contractor_GenerateReport_ReturnsCorrectReport()
+    {
+        var contractor = new Contractor("Fred", 50m, 40m);
+
+        string report = contractor.GenerateReport();
+
+        Assert.Contains("Fred", report);
+        Assert.Contains("50.00", report);
+        Assert.Contains("40", report);
+        Assert.Contains("1600.00", report);
+    }
+}
