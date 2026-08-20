95d5fb16f951eb9c4b1d0f71b29e83392d927aa2
Lu Yuchen <158829614@qq.com>
2026-08-20 15:10:22 +0930

    Named FullTimeEmployee and CalculatePay

--- Week3/Prac B/EmployeeApp/FullTimeEmployee.cs
+++ Week3/Prac B/EmployeeApp/FullTimeEmployee.cs
@@ -2,5 +2,15 @@ public class FullTimeEmployee : Employee, IReportable
 {
     public decimal AnnualSalary { get; set; }
 
-    
+    public FullTimeEmployee(string name, decimal annualSalary)
+        : base(name)
+    {
+        AnnualSalary = annualSalary;
+    }
+
+    public override decimal CalculatePay()
+    {
+        decimal tax = AnnualSalary * TaxRate;
+        return AnnualSalary - tax;
+    }
 }
