39838b0ef9d9b42ce5a41891264dae7ee9a9203b
Lu Yuchen <158829614@qq.com>
2026-08-20 15:20:01 +0930

    Add program

--- Week3/Prac B/EmployeeApp/Program.cs
+++ Week3/Prac B/EmployeeApp/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿FullTimeEmployee employee =
+    new FullTimeEmployee("Jordan", 90000m);
+
+Contractor contractor =
+    new Contractor("Sarah", 75m, 35m);
+
+Console.WriteLine(employee.GenerateReport());
+Console.WriteLine();
+
+Console.WriteLine(contractor.GenerateReport());
+Console.WriteLine();
+
+Console.WriteLine($"Employee Pay: ${employee.CalculatePay():F2}");
+Console.WriteLine($"Contractor Pay: ${contractor.CalculatePay():F2}");
