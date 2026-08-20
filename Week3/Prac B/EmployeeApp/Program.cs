896036ebcfc521c52e3f7e35b89122db7c73b931
Lu Yuchen <158829614@qq.com>
2026-08-20 15:32:30 +0930

    Change Program.cs

--- Week3/Prac B/EmployeeApp/Program.cs
+++ Week3/Prac B/EmployeeApp/Program.cs
@@ -1,14 +1,13 @@
-﻿FullTimeEmployee employee =
-    new FullTimeEmployee("Jordan", 90000m);
+﻿List<Employee> employees = new List<Employee>
+{
+    new FullTimeEmployee("Bill", 60000m),
+    new Contractor("Fred", 50m, 40m)
+};
 
-Contractor contractor =
-    new Contractor("Sarah", 75m, 35m);
+foreach (Employee employee in employees)
+{
+    decimal pay = employee.CalculatePay();
+    decimal tax = pay / (1 - Employee.TaxRate) * Employee.TaxRate;
 
-Console.WriteLine(employee.GenerateReport());
-Console.WriteLine();
-
-Console.WriteLine(contractor.GenerateReport());
-Console.WriteLine();
-
-Console.WriteLine($"Employee Pay: ${employee.CalculatePay():F2}");
-Console.WriteLine($"Contractor Pay: ${contractor.CalculatePay():F2}");
+    Console.WriteLine($"{employee.Name}: Pay ${pay:F2}. Tax ${tax:F2}.");
+}
