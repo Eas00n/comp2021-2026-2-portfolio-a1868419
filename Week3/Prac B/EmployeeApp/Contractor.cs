a859ca147f17530d9ff4a96d9a0f80d066dd95a9
Lu Yuchen <158829614@qq.com>
2026-08-20 15:13:47 +0930

    Contractor

--- Week3/Prac B/EmployeeApp/Contractor.cs
+++ Week3/Prac B/EmployeeApp/Contractor.cs
@@ -2,4 +2,11 @@ public class Contractor : Employee, IReportable
 {
     public decimal Rate { get; set; }
     public decimal Hours { get; set; }
+
+    public Contractor(string name, decimal rate, decimal hours)
+        : base(name)
+    {
+        Rate = rate;
+        Hours = hours;
+    }
 }
